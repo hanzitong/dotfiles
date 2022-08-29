0dec57ed3059d9199eb7bdba560451014d68d960
Han Zitong <han.zitong@rm.is.tohoku.ac.jp>
2022-08-28 23:43:49 -0700

    del practice script, and made 'meke_symlink.sh'

---
+++ scripts/make_symlink.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "start make symlink to dotfiles"
+
+
+
+FILES=(gitconfig vimrc tmux.conf)
+
+for file in $FILES[@]
+do
+    echo "file"
+    ln -s 
+done
+
+
+
+
+
+
+
--- scripts/practice_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-read -p "Input your name:" name
-echo "Hello $name"
-
-
-
-
-
