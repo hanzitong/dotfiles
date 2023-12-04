86c737dc7e6b36de8c224211d1096536ec450fc4
Han Zitong <han.zitong@rm.is.tohoku.ac.jp>
2023-12-04 17:06:45 +0900

    add shortcuts.sh

---
+++ scripts/shortcuts.sh
@@ -0,0 +1,5 @@
+## unable unneccesary shortcut for HHKB
+# gsettings list-recursively | grep Alt |grep Above     // to confirm shortcuts
+gsettings set org.gnome.desktop.wm.keybindings switch-group []
+
+
